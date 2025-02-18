dbebfbcb302a1df6e3e8e59e9977acea41eab8d7
weiling <10936081+weiling59ling@user.noreply.gitee.com>
2025-02-18 22:53:35 +0800

    feat:配置服务器状态码以及中间件

--- src/controller/test.js
+++ src/controller/test.js
@@ -4,5 +4,6 @@ module.exports = (req,res, next)=>{
 const testData = test()
 testData.desc = '自定义测试数据'
 console.log("testData=====>>> ",testData);
-res.json(testData)
+// res.success(testData)
+res.fail('请求失败')
 }
